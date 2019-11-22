dddf7a4ca59b1d6c58e8f95deb9b85f9475174b7
james <jamesbury77@gmail.com>
2019-11-22 08:37:21 -0500

    Added test comments

--- src/tests/TreeTests.java
+++ src/tests/TreeTests.java
@@ -12,6 +12,10 @@ import dataStructures.ListBuilder;
 
 class TreeTests {
 
+	/*
+	 * Simple test to make sure the contains method
+	 * works
+	 */
 	@Test
 	void containsTest() {
 
@@ -27,21 +31,31 @@ class TreeTests {
 			"eaYQmsyEfNWLtXTieRNReVyUJ"
 		};
 		
+		// Check contains right after insert
 		for(String v : values) {
 			assertTrue(n.insertNode(new BSTNode(v)));
 			assertTrue(n.contains(v));
 		}
+		
+		// Check contains again on every value
+		for(String v : values) {
+			assertTrue(n.contains(v));
+		}
+		
+		// Check a string that is not in the list
 		assertFalse(n.contains("not present"));
 	}
 	
+	/*
+	 * Tests contains on a list of 1000 strings
+	 */
 	@Test
 	void bigListTest() {
 		ListBuilder lb = new ListBuilder();
-        String[] arr = lb.getList(1000);
-        
-        BSTSearcher searcher = new BSTSearcher(arr);
-        searcher.findItem(true);
-        
+		String[] arr = lb.getList(1000);
+		
+		BSTSearcher searcher = new BSTSearcher(arr);
+		searcher.findItem(true);
+		
 	}
-
 }
